823e46900ba332e8874d54917f43f4effcd41b78
Mats Rauhala <mats.rauhala@iki.fi>
2019-01-01 15:30:23 +0200

    Shell support

--- release.nix
+++ release.nix
@@ -1,5 +1,15 @@
 { nixpkgs ? import ./nixpkgs.nix {} }:
 
-{
+rec {
   imageduplicates = nixpkgs.haskellPackages.callPackage ./default.nix {};
+  shell = nixpkgs.buildEnv {
+    name = "shell";
+    paths = [];
+    buildInputs = with nixpkgs.haskellPackages; [
+      ghcid
+      hasktags
+      cabal-install
+      (ghcWithPackages (_: imageduplicates.buildInputs ++ imageduplicates.propagatedBuildInputs))
+    ];
+  };
 }
