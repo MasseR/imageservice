fb112759ef441cd9d63cf6d8c1b519ab8732fd19
Mats Rauhala <mats.rauhala@iki.fi>
2019-01-27 14:57:35 +0200

    Batch requests

--- src/Worker/Indexer.hs
+++ src/Worker/Indexer.hs
@@ -32,7 +32,8 @@ indexer = do
   forever $ do
     forM_ ws $ \case
       Reddit subs ->
-        forM_ subs $ \sub -> Reddit.images (Reddit.Subreddit $ unpack sub) >>= mapM_ (push queue . Reddit.getImg)
+        -- forM_ subs $ \sub -> Reddit.images (Reddit.Subreddit $ unpack sub) >>= mapM_ (push queue . Reddit.getImg)
+        Reddit.images (map (Reddit.Subreddit . unpack) subs) >>= mapM_ (push queue . Reddit.getImg)
     liftIO (threadDelay (15 * 15 * 1000000))
   where
     push queue = liftIO . atomically . writeTChan queue
--- src/Worker/Indexer/Reddit.hs
+++ src/Worker/Indexer/Reddit.hs
@@ -13,18 +13,22 @@ import           Control.Monad.Logger
 import           Data.Aeson.Lens
 import           Data.Generics.Product
 import           Network.HTTP.Client
+import Data.List.Split (chunksOf)
 
-newtype Subreddit = Subreddit String
+newtype Subreddit = Subreddit { getSubreddit :: String }
 newtype ImgHref = ImgHref { getImg :: String }
 
 -- Consider going to full types, they're just a bit complex given the time I have
 
-images :: (MonadLogger m, HasType Manager r, MonadThrow m, MonadReader r m, MonadUnliftIO m) => Subreddit -> m [ImgHref]
-images (Subreddit r) = do
-  $logInfo $ "Fetching images for " <> tshow r
-  manager <- view (typed @Manager)
-  lbs <- getLbs manager ("https://www.reddit.com/r/" <> r <> ".json")
-  return . map (ImgHref . unpack) . filter (\x -> ".jpg" `isSuffixOf` x || ".png" `isSuffixOf` x) . getImages $ lbs
+images :: (MonadLogger m, HasType Manager r, MonadThrow m, MonadReader r m, MonadUnliftIO m) => [Subreddit] -> m [ImgHref]
+images rs = concat <$> mapM multireddit (mkMultireddit rs)
   where
+    multireddit r = do
+      $logInfo $ "Fetching images for " <> tshow r
+      manager <- view (typed @Manager)
+      lbs <- getLbs manager ("https://www.reddit.com/r/" <> r <> ".json")
+      return . map (ImgHref . unpack) . filter (\x -> ".jpg" `isSuffixOf` x || ".png" `isSuffixOf` x) . getImages $ lbs
     getImages json = json ^.. key "data" . key "children" . _Array . traverse . key "data" . key "url" . _String
+    mkMultireddit :: [Subreddit] -> [String]
+    mkMultireddit = map (intercalate "+") . chunksOf 5 . map getSubreddit
 
