3a886d90f1b176bb22e07f812eb00ea3279d25fd
Mats Rauhala <mats.rauhala@iki.fi>
2019-04-17 21:49:27 +0300

    Make the builds work on stable and unstable

--- nixpkgs.nix
+++
@@ -1,8 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-
-import (nixpkgs.fetchFromGitHub {
-  owner = "nixos";
-  repo = "nixpkgs";
-  rev = "bcab3daac7e1829de27f80ae18669607f6c19fdf";
-  sha256 = "0dw9d0sx66fp2argsprrg2vl0kzji1drl69ji5fvyngwfad23l9x";
-}) {}
--- release.nix
+++ release.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import ./nixpkgs.nix {} }:
+{ nixpkgs ? import <nixpkgs> {} }:
 
 rec {
   imageservice = nixpkgs.haskellPackages.callPackage ./default.nix {};
