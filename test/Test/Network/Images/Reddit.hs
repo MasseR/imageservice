9fe15c87312541f7a917a0866f9c9fa78817d918
Mats Rauhala <mats.rauhala@iki.fi>
2019-10-25 22:35:41 +0300

    Fix tests
    - Modified the html handling

--- test/Test/Network/Images/Reddit.hs
+++ test/Test/Network/Images/Reddit.hs
@@ -18,11 +18,7 @@ removeRejects = filter $ \case
 
 spec :: Spec
 spec = describe "Images.Html" $ do
-  it "should return no results for empty json" $ do
-    lbs <- view (from strict) <$> readFile "testdata/empty.json"
-    urls <- evalTestHTTP (indexer "http://example.com") lbs
-    removeRejects urls `shouldBe` []
   it "should return results for non-empty json" $ do
     lbs <- view (from strict) <$> readFile "testdata/data.json"
     urls <- evalTestHTTP (indexer "http://example.com") lbs
-    length (removeRejects urls) `shouldBe` 24
+    length (removeRejects urls) `shouldBe` 25
