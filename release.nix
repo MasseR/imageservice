2e2232575c2c5e1f719edc241f2d5dadceed464a
Mats Rauhala <mats.rauhala@iki.fi>
2019-12-18 21:21:33 +0200

    Use the prelude from 'masse-prelude'

--- release.nix
+++ release.nix
@@ -1,12 +1,25 @@
 { nixpkgs ? import <nixpkgs> {} }:
 
+let
+  hp = nixpkgs.haskellPackages.extend (self: super: {
+    masse-prelude = self.callPackage (nixpkgs.fetchFromGitHub {
+      owner = "MasseR";
+      repo = "masse-prelude";
+      rev = "ab431de3e5b091d9277d745e6e22e051573e8cae";
+      sha256 = "0036rp5c1wfl68iq198zycj3avq2l0hxwnhjwp29ybi8q7pk3qfq";
+    }) {};
+  });
+
+in
+
 rec {
-  imageservice = nixpkgs.haskellPackages.callPackage ./default.nix {};
+  inherit (hp) masse-prelude;
+  imageservice = nixpkgs.callPackage ./default.nix { haskellPackages = hp; };
   frontend = (nixpkgs.callPackage ./frontend {}).frontend;
   shell = nixpkgs.buildEnv {
     name = "shell";
     paths = [];
-    buildInputs = with nixpkgs.haskellPackages; [
+    buildInputs = with hp; [
       hlint
       stylish-haskell
       ghcid
